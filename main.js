c9f096855fe22761a19da93424550d23198325a6
Casyoo <116643332+Casyoo@users.noreply.github.com>
2023-03-16 19:52:46 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//JQuery used for hover effect on Nav Links
+$('.liNavLinks').on('mouseenter',function(){
+    $(this).css('backgroundColor','#e9d985');
+    }
+);
+
+$('.liNavLinks').on('mouseleave',function(){
+    $(this).css('backgroundColor','#222e50');
+    }
+);
+
+
+/*
+Bibliography and Resources:
+Icons and images:
+
+1.  dooley, ian (1997) The things you need, Unsplash. Available at: https://unsplash.com/photos/DJ7bWa-Gwks (Accessed: March 16, 2023). 
+2.  Firmbee.com (2015) woman holding an iphone, Unsplash. Available at: https://unsplash.com/photos/SpVHcbuKi6E (Accessed: March 16, 2023). 
+3.  Henry B (2018) The Library of Trinity College, Unsplash. Available at: https://unsplash.com/photos/lc7xcWebECc (Accessed: March 16, 2023). 
+4.  Nygård, A. (2021). Unsplash.Available at: https://unsplash.com/photos/h7-V2KkCECI (Accessed: March 16, 2023). 
+5.  Studio, U. (2015) Designer sketching Wireframes, Unsplash. Available at: https://unsplash.com/photos/s9CC2SKySJM (Accessed: March 16, 2023). 
+6.  “Icons” (no date). Icon Duck. 
+
+API
+
+1.  jQuery (no date) Google Hosted Library. Google. Available at: https://developers.google.com/speed/libraries#jquery (Accessed: March 16, 2023). 
+
+Resources:
+
+1.  w3schools (no date) CSS @media Rule, CSS @media rule. Available at: https://www.w3schools.com/cssref/css3_pr_mediaquery.php (Accessed: March 16, 2023). 
+2.  w3schools (no date) CSS box-shadow property. Available at: https://www.w3schools.com/cssref/css3_pr_box-shadow.php (Accessed: March 16, 2023). 
+3.  w3schools (no date) CSS grid layout module, CSS Grid Layout. Available at: https://www.w3schools.com/css/css_grid.asp (Accessed: March 16, 2023). 
+4.  w3schools (no date) CSS text-shadow property. Available at: https://www.w3schools.com/cssref/css3_pr_text-shadow.php (Accessed: March 16, 2023). 
+
+*/
+
+
+
